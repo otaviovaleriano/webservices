02d76f651d27d7fdb2a4bf21a903e2686b6dec0e
Otavio Silva <otaviovalerianoss@gmail.com>
2024-01-20 18:36:30 -0700

    personal assignment

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const routes = require('express').Router();
 
-const myController = require('../controllers');
+// const myController = require('../controllers');
+// routes.get('/more', myController.returnAnotherPerson);
 
-routes.get('/', myController.awesomeFunction);
-routes.get('/more', myController.returnAnotherPerson);
+routes.use('/contacts', require('./contacts'));
+
+
 
 module.exports = routes;
