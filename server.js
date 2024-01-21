1c12af163ee9fb14aee9a945c4d674ac1e81cc74
Otavio Silva <otaviovalerianoss@gmail.com>
2024-01-20 17:36:25 -0700

    personal assignment

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
+const mongodb = require('./db/connect');
 
 app.use('/', require('./routes/index'));
 
 app.listen(3000, () => {
     console.log(`server started on port ${port}`);
+});
+
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('mongodb connected');
+    }
 });
