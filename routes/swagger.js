c65e43a56c51ec3db1e538367a88afbebde7f0ec
Otavio Silva <otaviovalerianoss@gmail.com>
2024-02-03 16:18:04 -0700

    testing again

--- routes/swagger.js
+++ routes/swagger.js
@@ -3,6 +3,6 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 router.use('/api-docs', swaggerUi.serve);
 // router.get('/api-docs', swaggerUi.setup(swaggerDocument));
-router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.get('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
