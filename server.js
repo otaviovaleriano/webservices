4ad748daf5e1a2bc3ba037f9f7223f32067c4b76
Otavio Silva <otaviovalerianoss@gmail.com>
2024-02-03 16:53:51 -0700

    testing suggestions

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
-const express = require('express');
+var express = require('express');
 const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
-
+var app = express();
+app.use(express.json());
 const port = process.env.PORT || 8080;
-const app = express();
+const mongodb = require('./db/connect');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('swagger.json');
+
 
 app
-  .use(bodyParser.json())
+//   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(bodyParser.json())  
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-type");
+    // res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+     
     next();
   })
-  .use('/', require('./routes'));
+  .use('/', require('./routes/index'));
 
-mongodb.initDb((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
+
+// app.listen(8080, () => {
+//     console.log(`server started on port ${port}`);
+// });
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        //app.listen(port);
+        console.log(`Connected to DB and listening on ${port}`);
+    }
 });
